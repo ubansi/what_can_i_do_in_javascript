ed80d39c6fc0b4bd486977bef90ab38c5c23ddec
ubansi <goakafu@gmail.com>
2017-08-14 02:44:19 +0900

    add node script

---
+++ fizz_buzz_module.js
@@ -0,0 +1,21 @@
+function getFizzBuzzAns(num) {
+    var FIZZ = 'Fizz';
+    var BUZZ = 'Buzz';
+
+    var isFizz = num % 3 === 0;
+    var isBuzz = num % 5 === 0;
+
+    if (isBuzz && isFizz) {
+        return FIZZ + BUZZ;
+    }
+    if (isFizz) {
+        return FIZZ;
+    }
+    if (isBuzz) {
+        return BUZZ;
+    }
+
+    return num;
+}
+
+exports.getFizzBuzzAns = getFizzBuzzAns;
---
+++ nodejs/node_script.js
@@ -0,0 +1,14 @@
+var fb = require('../fizz_buzz_module.js');
+var readline = require('readline');
+var reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+reader.on('line', function (line) {
+    console.log(line);
+    var max = line;
+    for (let num = 0; num <= max; num++) {
+        console.log(fb.getFizzBuzzAns(num));
+    }
+});
